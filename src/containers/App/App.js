6574a646182ebbd24105fa75563814ddff2b7e3d
Abzeede(Bank) <purijit@satang.com>
2019-06-02 13:35:42 +0700

    make ui/ux more beautiful

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TodoForm from 'components/TodoForm'
 import TodoItemList from 'components/TodoItemList'
 import ProgressBar from 'components/ProgressBar'
+import './style.scss'
 
 class App extends React.Component {
   state = {
@@ -47,22 +48,48 @@ class App extends React.Component {
     return this.state.items.filter(item => item.done === done)
   }
 
+  onEdit = (id, detail) => {
+    this.setState(state => ({
+      items: state.items.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            detail: detail,
+          }
+        }
+        return item
+      }),
+    }))
+  }
+
   render() {
     const todoItems = this.getTodoItemsByStatus({ done: false })
     const doneItems = this.getTodoItemsByStatus({ done: true })
 
     return (
-      <div>
-        <TodoForm onSubmit={this.onCreateTodo} placeholder="Add a to-do..." />
-        <ProgressBar items={this.state.items} showPercent />
-        <TodoItemList
-          items={todoItems}
-          onDelete={this.onDelete}
-          onToggleDone={this.onToggleDone}
-          onEdit={this.onEdit}
-        />
-        <div>done</div>
-        <TodoItemList items={doneItems} onDelete={this.onDelete} onToggleDone={this.onToggleDone} />
+      <div className="app">
+        <div className="app__todo-form">
+          <TodoForm onSubmit={this.onCreateTodo} placeholder="Add a todo..." />
+        </div>
+        {todoItems.length !== 0 && (
+          <>
+            <div className="app__progress-bar">
+              <ProgressBar items={this.state.items} showPercent />
+            </div>
+            <TodoItemList
+              items={todoItems}
+              onDelete={this.onDelete}
+              onToggleDone={this.onToggleDone}
+              onEdit={this.onEdit}
+            />
+          </>
+        )}
+        {doneItems.length !== 0 && (
+          <>
+            <h3 className="app__label">Done</h3>
+            <TodoItemList items={doneItems} onDelete={this.onDelete} onToggleDone={this.onToggleDone} />
+          </>
+        )}
       </div>
     )
   }

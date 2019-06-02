55de9d33c3236d3753956470f6b7bfdd7348a165
Abzeede(Bank) <purijit@satang.com>
2019-06-02 12:12:12 +0700

    implement core todo logic

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,77 @@
 import React from 'react'
 
 class App extends React.Component {
+  state = {
+    items: [],
+    todo: '',
+  }
+
+  setTodo = todo => this.setState({ todo })
+
+  generateUID = () => new Date().getTime()
+
+  onCreateTodo = todo => {
+    this.setState(state => ({
+      items: [
+        {
+          id: this.generateUID(),
+          detail: todo,
+          done: false,
+        },
+        ...state.items,
+      ],
+    }))
+  }
+
+  onDelete = id => {
+    this.setState(state => ({
+      items: state.items.filter(item => item.id !== id),
+    }))
+  }
+
+  onToggleDone = id => {
+    this.setState(state => ({
+      items: state.items.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            done: !item.done,
+          }
+        }
+        return item
+      }),
+    }))
+  }
+
+  getTodoItemsByStatus = ({ done }) => {
+    return this.state.items.filter(item => item.done === done)
+  }
+
   render() {
+    const todoItems = this.getTodoItemsByStatus({ done: false })
+    const doneItems = this.getTodoItemsByStatus({ done: true })
+
     return (
       <div>
-        <div>Todo Form</div>
+        <input onChange={e => this.setTodo(e.target.value)} value={this.state.todo} />
+        <button onClick={() => this.onCreateTodo(this.state.todo)}>create todo</button>
         <div>ProgressBar</div>
-        <div>Todo list</div>
-        <div>Done list</div>
+        <div>todo</div>
+        <ul>
+          {todoItems.map(item => (
+            <li key={item.id} onClick={() => this.onToggleDone(item.id)}>
+              {item.detail} <button onClick={() => this.onDelete(item.id)}>delete</button>
+            </li>
+          ))}
+        </ul>
+        <div>done</div>
+        <ul>
+          {doneItems.map(item => (
+            <li key={item.id} onClick={() => this.onToggleDone(item.id)}>
+              {item.detail} <button onClick={() => this.onDelete(item.id)}>delete</button>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }

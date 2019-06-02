d06af29989662670ab209fc6dcde7ed8b3e867d5
Abzeede(Bank) <purijit@satang.com>
2019-06-02 15:49:52 +0700

    upload build folder

---
+++ build/precache-manifest.79d2a77601748174791105da4adb7bf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '32dca7781522e7b459b55e1caf25e4f6',
+    url: '/index.html',
+  },
+  {
+    revision: '9eb1c8ce6d7c7efb1e98',
+    url: '/static/css/main.d6d97596.chunk.css',
+  },
+  {
+    revision: 'd132446a14439e95ed4b',
+    url: '/static/js/2.7f5317ab.chunk.js',
+  },
+  {
+    revision: '9eb1c8ce6d7c7efb1e98',
+    url: '/static/js/main.65c30a55.chunk.js',
+  },
+  {
+    revision: 'fdfcfda2d9b1bf31db52',
+    url: '/static/js/runtime~main.c5541365.js',
+  },
+])

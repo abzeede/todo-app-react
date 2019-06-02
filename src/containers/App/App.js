ab63296b8d890e8fa2b3225f8ef9212060f94fc8
Abzeede(Bank) <purijit@satang.com>
2019-06-02 11:58:44 +0700

    init app layout

---
+++ src/containers/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Todo Form</div>
+        <div>ProgressBar</div>
+        <div>Todo list</div>
+        <div>Done list</div>
+      </div>
+    )
+  }
+}
+
+export default App

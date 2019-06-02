db88f84c43f312cf5349505596c0f47816172fe5
Abzeede(Bank) <purijit@satang.com>
2019-06-03 01:11:48 +0700

    Updates

---
+++ precache-manifest.1a956d6a8fa996b10ce74dde29c3bdb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'c40ca5bfc67b584ca94fbb6bfb564808',
+    url: '/todo-app-react/index.html',
+  },
+  {
+    revision: '8d50a865ff462bdb796e',
+    url: '/todo-app-react/static/css/main.2f4731db.chunk.css',
+  },
+  {
+    revision: 'd132446a14439e95ed4b',
+    url: '/todo-app-react/static/js/2.7f5317ab.chunk.js',
+  },
+  {
+    revision: '8d50a865ff462bdb796e',
+    url: '/todo-app-react/static/js/main.c70ef718.chunk.js',
+  },
+  {
+    revision: '71507c0fe40142d1c314',
+    url: '/todo-app-react/static/js/runtime~main.09986612.js',
+  },
+])
